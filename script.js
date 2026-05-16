3991106b2e57fd68c536f9b015c5a5cedcd1958a
Dumindu Wickramaarachchi <dumindu834@gmail.com>
2026-05-16 10:25:54 +0530

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,52 @@
+AOS.init();
+
+var typed = new Typed(".typing", {
+
+    strings: [
+
+        "AI Engineer",
+        "Machine Learning Enthusiast",
+        "Business Analyst",
+        "SQL Developer",
+        "Dashboard Developer"
+
+    ],
+
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+
+});
+
+particlesJS("particles-js", {
+
+    particles: {
+
+        number: {
+            value: 80
+        },
+
+        color: {
+            value: "#00f7ff"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.5
+        },
+
+        size: {
+            value: 3
+        },
+
+        move: {
+            enable: true,
+            speed: 2
+        }
+
+    }
+
+});
